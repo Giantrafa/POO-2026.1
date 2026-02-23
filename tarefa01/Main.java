e1ea9e16aa6123f2c373225a16db09d40ffde922
Giantrafa <giantrafa@gmail.com>
2026-02-23 09:50:16 -0300

    quadrado

--- tarefa01/Main.java
+++ tarefa01/Main.java
@@ -2,8 +2,13 @@ package tarefa01;
 
 public class Main{
     public static void main (String[] args){
-        Quadrado r = new Quadrado();
+        Quadrado r = new Quadrado(4);
         
-        System.out.println(r.area());
+        for(int i = 0; i < r.lado; i++){
+            for(int j = 0; j < r.lado; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
