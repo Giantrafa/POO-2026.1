5e90fc6462016188fd3e028c4b36ecbc7b27c0e6
Giantrafa <giantrafa@gmail.com>
2026-02-23 10:00:55 -0300

    mostra a diferença usando Triangulo
    Added instances of Triangulo and display logic.

--- tarefa02/Main2.java
+++ tarefa02/Main2.java
@@ -2,6 +2,15 @@ package tarefa02;
 
 public class Main2 {
     public static void main(String[] args) {
-    //depois
- }   
+        Triangulo t1 = new Triangulo(5);          
+        Triangulo t2 = new Triangulo(5, 8);       
+        Triangulo t3 = new Triangulo(3, 4, 5); 
+
+        // Exibindo informações
+        t1.exibe();
+        System.out.println();
+        t2.exibe();
+        System.out.println();
+        t3.exibe();
+    }   
 }
